8db50472741835fe35ff0fa17744b8fcce4efe56
niammbr <nikhil@ammbrtech.com>
2021-04-18 16:50:00 +0530

    Upgrade

--- dist/widgets/Menu/components/Panel.d.ts
+++ dist/widgets/Menu/components/Panel.d.ts
@@ -1,5 +1,5 @@
-import React from "react";
-import { PanelProps, PushedProps } from "../types";
+import { PanelProps, PushedProps } from '../types';
+import React from 'react';
 interface Props extends PanelProps, PushedProps {
     showMenu: boolean;
     isMobile: boolean;
